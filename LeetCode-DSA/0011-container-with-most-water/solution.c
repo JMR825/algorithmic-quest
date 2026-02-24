e00c7f968e9d85eb7391dea52575a1a082e4c895
JMR <152759615+JMR825@users.noreply.github.com>
2026-02-24 12:22:03 +0000

    Sync LeetCode submission Runtime - 1 ms (39.01%), Memory - 14.3 MB (100.00%)

---
+++ LeetCode-DSA/0011-container-with-most-water/solution.c
@@ -0,0 +1,19 @@
+int maxArea(int* height, int heightSize) {
+    int i = 0;
+    int j = heightSize - 1;
+    int res = 0;
+
+    while (i < j) {
+        int minHeight = height[i] < height[j] ? height[i] : height[j];
+        int area = (j - i) * minHeight;
+        if (area > res) res = area;
+
+        if (height[i] < height[j]) {
+            i++;
+        } else {
+            j--;
+        }
+    }
+
+    return res;
+}
