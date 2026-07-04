6b982ddb06e12039b7a576e4e98e203ecfb55dcb
JMR <152759615+JMR825@users.noreply.github.com>
2026-07-04 05:37:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.9 MB (64.02%)

--- LeetCode-DSA/0908-middle-of-the-linked-list/solution.cpp
+++ LeetCode-DSA/0908-middle-of-the-linked-list/solution.cpp
@@ -11,16 +11,14 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        
-
-    ListNode* slow=head;
-    ListNode* fast=head;
-    while(fast!=nullptr && fast->next!=nullptr)
-    {
-        slow=slow->next;
-        fast=fast->next->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=nullptr && fast->next!=nullptr)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        if(slow==nullptr) return 0;
+        return slow;
     }
-    return slow;   
-    
-}
 };
