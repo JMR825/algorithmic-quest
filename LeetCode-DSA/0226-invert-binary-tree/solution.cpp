03e721813c2a7b434b98df6ae0d39bf1b719e341
JMR <152759615+JMR825@users.noreply.github.com>
2026-07-29 07:21:11 +0000

    Sync LeetCode submission Runtime - 2 ms (0.78%), Memory - 12.5 MB (26.85%)

--- LeetCode-DSA/0226-invert-binary-tree/solution.cpp
+++ LeetCode-DSA/0226-invert-binary-tree/solution.cpp
@@ -11,13 +11,20 @@
  */
 class Solution {
 public:
+void dfs(TreeNode* root)
+{
+    if(root==nullptr) return;
+    TreeNode* temp = root->left;
+root->left = root->right;
+root->right = temp;
+dfs(root->left);
+dfs(root->right);
+
+}
     TreeNode* invertTree(TreeNode* root) {
-        if(root==nullptr) return root;
-        TreeNode* Temp=root->right;
-        root->right=root->left;
-        root->left=Temp;
-        invertTree(root->left);
-        invertTree(root->right);
+
+        dfs(root);
+
         return root;
     }
 };
