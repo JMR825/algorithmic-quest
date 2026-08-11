14b94f8661dbe91d0db98aeee76c8290e795b424
JMR <152759615+JMR825@users.noreply.github.com>
2026-08-11 11:56:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.3 MB (37.97%)

---
+++ LeetCode-DSA/0048-rotate-image/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for(int offset =0;offset<last-first;offset++){
+                //int top= matrix[first][first+offset];
+                //int right = matrix[first+ offset][last];
+                //int bottom = matrix[last][last-offset];
+                //int left =  matrix[last-offset][first];
+                int temp=matrix[first][first+offset];
+                matrix[first][first+offset]= matrix[last-offset][first];
+                matrix[last-offset][first]=matrix[last][last-offset];
+                matrix[last][last-offset]=  matrix[first+ offset][last];
+                matrix[first+ offset][last]= temp;
+            }
+        }
+    }
+};
